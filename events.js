c8d863019185955aedb1146bbea492446d69038a
Peter Law <PeterJCLaw@gmail.com>
2026-04-08 21:17:10 +0100

    Reload the matches cache when it changes
    Fixes https://github.com/srobo/livestream-overlay/issues/33

--- events.js
+++ events.js
@@ -28,6 +28,12 @@ if (!!window.EventSource) {
     }
   }, false);
 
+  eventSource.addEventListener('matches', function(e){
+    if (typeof onMatchesUpdate == 'function') {
+        onMatchesUpdate(JSON.parse(e.data));
+    }
+  }, false);
+
   eventSource.addEventListener('match', function(e){
     if (typeof onMatchUpdate == 'function') {
         onMatchUpdate(JSON.parse(e.data));
